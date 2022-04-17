e1b219965e246b3faa3f5db684a90d6dd7614022
FaisalMashuri <faisal.mashuri18143@student.unsika.ac.id>
2022-04-17 21:00:34 +0700

    finish making title in detail page

--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -24,19 +24,80 @@ class DetailPage extends StatelessWidget {
       return Container(
         width: double.infinity,
         height: 214,
-        margin: EdgeInsets.only(top:236),
+        margin: EdgeInsets.only(top: 236),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
+            borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(35),
                 bottomRight: Radius.circular(35)),
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              kWhiteColor.withOpacity(0),
-              Colors.black.withOpacity(0.95)
-            ]
-          )
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  kWhiteColor.withOpacity(0),
+                  Colors.black.withOpacity(0.95)
+                ])),
+      );
+    }
+
+    Widget content() {
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: defaultMargin, vertical: 30),
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              width: 108,
+              height: 24,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/icon_emblem.png"))),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 256),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Lake Ciliwung",
+                          style: whiteTextStyle.copyWith(
+                              fontSize: 24, fontWeight: semiBold),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          "Tangerang",
+                          style: whiteTextStyle.copyWith(
+                              fontSize: 16, fontWeight: light),
+                        )
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 2),
+                        width: 25,
+                        height: 25,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("assets/icon_star.png"))),
+                      ),
+                      Text(
+                        "4.5",
+                        style: whiteTextStyle.copyWith(
+                            fontWeight: medium, fontSize: 20),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       );
     }
@@ -44,10 +105,7 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: Stack(
-        children: [
-          backgroundImage(),
-          customShadow()  
-        ],
+        children: [backgroundImage(), customShadow(), content()],
       ),
     );
   }
